3d743a3f89beb574625acf3e32d2613b88a5da3e
ashnagul <amirandau@fen.uchile.cl>
2020-11-08 11:46:19 -0300

    Correction to all methods in producto and usuario

--- src/routers/producto.js
+++ src/routers/producto.js
@@ -5,35 +5,42 @@ const productoRouter = express.Router()
 productoRouter.get('/', (req, res) => {
     productos.findAll().then(producto => {
         res.json(producto)
+    }).catch(e => {
+        res.json(e)
     })
 })
 
 productoRouter.get('/:id', (req, res) => {
     productos.findOne({ where: req.params }).then(producto => {
         res.json(producto)
-    }).catch(error => res.json(error))
+    }).catch(e => {
+        res.json(error)
+    })
 })
 
 
 productoRouter.patch('/:id', (req,res) => {
     productos.update({ where: req.params }).then(producto => {
         res.json(producto)
+    }).catch(e => {
+        res.json(e)
     })
 })
 
 productoRouter.delete('/:id', (req,res) => {
     productos.destroy({ where: req.params }).then(producto => {
         res.json(producto)
+    }).catch(e => {
+        res.json(e)
     })
 })
 
 productoRouter.post('/', (req, res) => {
-    if(req.body.nombreProducto){
-        productos.create(req.body)
-        res.send({ Status: "Success" })
-    } else {
-        res.send({ Status: "Error" })
-    }
+    productos.create(req.body).then(producto => {
+        res.json(producto)
+    }).catch(e => {
+        res.json(e)
+    })
 })
 
 module.exports = productoRouter;
--- src/routers/usuario.js
+++ src/routers/usuario.js
@@ -6,42 +6,42 @@ const usuarioRouter = express.Router()
 usuarioRouter.get('/', (req, res) => {
     usuarios.findAll().then(usuario => {
         res.json(usuario)
+    }).catch(e => {
+        res.json(e)
     })
 })
 
 usuarioRouter.get('/:id', (req, res) => {
     usuarios.findOne({ where: req.params }).then(usuario => {
         res.json(usuario)
-    }).catch(error => res.json(error))
+    }).catch(e => {
+        res.json(e)
+    })
 })
 
 
 usuarioRouter.patch('/:id', (req,res) => {
     usuarios.update({ where: req.params }).then(usuario => {
         res.json(usuario)
+    }).catch(e => {
+        res.json(e)
     })
 })
 
 usuarioRouter.delete('/:id', (req,res) => {
     usuarios.destroy({ where: req.params }).then(usuario => {
         res.json(usuario)
+    }).catch(e => {
+        res.json(e)
     })
 })
 
 usuarioRouter.post('/', (req, res) => {
-    if(req.body.username){
-        usuarios.findOne({ where: { [Op.or] : [{username: req.body.username},{email: req.body.email}]}}).then(usuario =>{
-            const usuarioEncontrado = usuario
-            if(usuarioEncontrado === null){
-                usuarios.create(req.body)
-                res.send({ Status: "Success" })
-            } else {
-                res.send({ Status: "Ya existe usuario o email"})
-            }
-        })
-    } else {
-        res.send({ Status: "Input no tiene formato requerido" })
-    }
+    usuarios.create(req.body).then(usuario => {
+        res.json(usuario)
+    }).catch(e => {
+        res.json(e)
+    })
 })
 
 
